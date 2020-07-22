78084ddcdf1550bfb25bc70217759c73527271a3
h9839 <60358181+h9839@users.noreply.github.com>
2020-07-22 17:52:40 -0600

    Update script.js

--- ext/src/browser_action/script.js
+++ ext/src/browser_action/script.js
@@ -1,1 +1,31 @@
 
+ 
+      var locationAPI = "https://api.wheretheiss.at/v1/satellites/25544";
+      
+     var interval = setInterval( getData , 1000);
+      
+      function getData(){
+      $.ajax({
+        url: locationAPI,
+        success: updateLocation
+        
+      });
+      }
+       
+        function updateLocation(response){
+
+$("#long-value").text(response.longitude);
+$("#lat-value").text(response.latitude);
+$("#count").text(response.daynum);
+          
+ $("#velocity").text(response.velocity);
+ $("#visibility").text(response.visibility);
+ $("#footprint").text(response.footprint);
+ $("#altitude").text(response.altitude);
+ $("#timestamp").text(response.timestamp);
+          
+          
+    
+}
+      
+
